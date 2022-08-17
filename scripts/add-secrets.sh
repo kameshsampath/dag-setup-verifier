2e5d368d54ed7955d3528404a7ea113503c6c3ce
Kamesh <kamesh.sampath@hotmail.com>
2022-08-17 15:40:05 +0530

    use values from registry

--- scripts/add-secrets.sh
+++ scripts/add-secrets.sh
@@ -2,8 +2,6 @@
 
 set -euxo pipefail
 
-drone secret add --name maven_mirror_url --data 'http://nexus.infra:8081/repository/maven-public/' "${GITEA_USERNAME}/dag-setup-verifier"
-
 drone secret add --name destination_image --data "${REGISTRY_NAME}/example/dag-setup-verifier" "${GITEA_USERNAME}/dag-setup-verifier"
 
 drone secret add --name image_registry --data "${REGISTRY_NAME}" "${GITEA_USERNAME}/dag-setup-verifier"
--- scripts/rm-secrets.sh
+++ scripts/rm-secrets.sh
@@ -2,12 +2,10 @@
 
 set -euxo pipefail
 
-drone secret rm --name maven_mirror_url "${GITEA_USERNAME}/dag-setup-verifier"
-
 drone secret rm --name destination_image "${GITEA_USERNAME}/dag-setup-verifier"
 
-drone secret rm --name image_registry
+drone secret rm --name image_registry "${GITEA_USERNAME}/dag-setup-verifier"
 
-drone secret rm --name image_registry_user
+drone secret rm --name image_registry_user "${GITEA_USERNAME}/dag-setup-verifier"
 
-drone secret update --name image_registry_password
+drone secret rm --name image_registry_password "${GITEA_USERNAME}/dag-setup-verifier"
