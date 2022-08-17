2bfcac1af5d0d05f57d20a35340a32d2118cb407
Kamesh <kamesh.sampath@hotmail.com>
2022-08-17 14:55:10 +0530

    add scripts

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def hello():
-  return '<h1>Jai Guru</h1>'
+  return '<h1>Hello from Captain Canary!!\uD83D\uDC25🚀</h1>'
 
 if __name__ == '__main__':
     app.run("0.0.0.0",8080)
---
+++ scripts/update-secrets.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+set -euxo pipefail
+
+drone secret update --name maven_mirror_url --data 'http://nexus.infra:8081/repository/maven-public/' "${GITEA_USERNAME}/dag-setup-verifier"
+
+drone secret update --name destination_image --data "${REGISTRY_NAME}/example/dag-setup-verifier" "${GITEA_USERNAME}/dag-setup-verifier"
+
+drone secret update --name image_registry --data "${REGISTRY_NAME}" "${GITEA_USERNAME}/dag-setup-verifier"
+
+drone secret update --name image_registry_user --data "${IMAGE_REGISTRY_USER}" "${GITEA_USERNAME}/dag-setup-verifier"
+
+drone secret update --name image_registry_password --data "${IMAGE_REGISTRY_PASSWORD}" "${GITEA_USERNAME}/dag-setup-verifier"
