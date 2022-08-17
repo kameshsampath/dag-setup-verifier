a6c03c1468f0f8f2eecf662a0f4f7b6dae73ba44
Kamesh <kamesh.sampath@hotmail.com>
2022-08-17 16:42:46 +0530

    trying again

--- scripts/rm-secrets.sh
+++ scripts/rm-secrets.sh
@@ -2,10 +2,10 @@
 
 set -euxo pipefail
 
-drone secret rm --name destination_image "${GITEA_USERNAME}/dag-setup-verifier"
+drone secret rm --name destination_image "${DRONE_GIT_REPO}"
 
-drone secret rm --name image_registry "${GITEA_USERNAME}/dag-setup-verifier"
+drone secret rm --name image_registry "${DRONE_GIT_REPO}"
 
-drone secret rm --name image_registry_user "${GITEA_USERNAME}/dag-setup-verifier"
+drone secret rm --name image_registry_user "${DRONE_GIT_REPO}"
 
-drone secret rm --name image_registry_password "${GITEA_USERNAME}/dag-setup-verifier"
+drone secret rm --name image_registry_password "${DRONE_GIT_REPO}"
--- scripts/update-secrets.sh
+++ scripts/update-secrets.sh
@@ -2,10 +2,10 @@
 
 set -euxo pipefail
 
-drone secret update --name destination_image --data "${REGISTRY_NAME}/example/dag-setup-verifier" "${GITEA_USERNAME}/dag-setup-verifier"
+drone secret update --name destination_image --data "${REGISTRY_NAME}/example/dag-setup-verifier" "${DRONE_GIT_REPO}"
 
-drone secret update --name image_registry --data "${REGISTRY_NAME}" "${GITEA_USERNAME}/dag-setup-verifier"
+drone secret update --name image_registry --data "${REGISTRY_NAME}" "${DRONE_GIT_REPO}"
 
-drone secret update --name image_registry_user --data "${IMAGE_REGISTRY_USER}" "${GITEA_USERNAME}/dag-setup-verifier"
+drone secret update --name image_registry_user --data "${IMAGE_REGISTRY_USER}" "${DRONE_GIT_REPO}"
 
-drone secret update --name image_registry_password --data "${IMAGE_REGISTRY_PASSWORD}" "${GITEA_USERNAME}/dag-setup-verifier"
+drone secret update --name image_registry_password --data "${IMAGE_REGISTRY_PASSWORD}" "${DRONE_GIT_REPO}"
